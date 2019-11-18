7e1f27d7c5cb5c5d1628c942c72eeca235402814
mlgomez0 <mlgomez230@hotmail.com>
2019-11-18 23:35:30 +0000

    Creation of exec handler function

---
+++ exec_handler.c
@@ -0,0 +1,15 @@
+#include"holberton.h"
+
+/**
+ *exec_handler: creates a child process and schedule execution
+ *@_args: arguments to use in the execution of the program
+ *Return: Integer 1 or 0
+ */
+
+int exec_handler(char **_args)
+{
+
+
+}
+
+
